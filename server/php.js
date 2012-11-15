6b2386fd4dd3d7ac9ee6cd2d73272efc1a085f09
Kevin Whitman <keverw92@gmail.com>
2012-11-15 15:14:03 -0500

    added md5, time and count functions

--- server/php.js
+++ server/php.js
@@ -1,4 +1,4 @@
-global.trim = function(str, charlist) {
+global.trim = function(str, charlist) { //Strip whitespace (or other characters) from the beginning and end of a string - http://php.net/manual/en/function.trim.php
 	//http://kevin.vanzonneveld.net
 	//+	original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
 	//+	improved by: mdsjack (http://www.mdsjack.bo.it)
@@ -45,4 +45,60 @@ global.trim = function(str, charlist) {
 	}
 
 	return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+}
+
+//Not from PHP.js, rewrote to use node.js native hash - http://stackoverflow.com/questions/6755107/how-to-make-hash-in-php-and-node-js-have-the-same-value
+global.md5 = function(str) { //Calculate the md5 hash of a string - http://php.net/manual/en/function.md5.php
+	var crypto = require('crypto');
+	return crypto.createHash('md5').update(str).digest('hex');
+}
+
+global.time = function() { //Return current Unix timestamp - http://php.net/manual/en/function.time.php
+	// http://kevin.vanzonneveld.net
+	// + original by: GeekFG (http://geekfg.blogspot.com)
+	// +	improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +	improved by: metjay
+	// +	improved by: HKM
+	// * example 1: timeStamp = time();
+	// * results 1: timeStamp > 1000000000 && timeStamp < 2000000000
+	return Math.floor(new Date().getTime() / 1000);
+}
+
+global.count = function(mixed_var, mode) { //Count all elements in an array, or something in an object - http://php.net/manual/en/function.count.php
+	// http://kevin.vanzonneveld.net
+	// + original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +	input by: Waldo Malqui Silva
+	// +	bugfixed by: Soren Hansen
+	// +	input by: merabi
+	// +	improved by: Brett Zamir (http://brett-zamir.me)
+	// +	bugfixed by: Olivier Louvignes (http://mg-crea.com/)
+	// * example 1: count([[0,0],[0,-4]], 'COUNT_RECURSIVE');
+	// *	returns 1: 6
+	// * example 2: count({'one' : [1,2,3,4,5]}, 'COUNT_RECURSIVE');
+	// *	returns 2: 6
+	var key, cnt = 0;
+
+	if (mixed_var === null || typeof mixed_var === 'undefined') {
+		return 0;
+	} else if (mixed_var.constructor !== Array && mixed_var.constructor !== Object) {
+		return 1;
+	}
+
+	if (mode === 'COUNT_RECURSIVE') {
+		mode = 1;
+	}
+	if (mode != 1) {
+		mode = 0;
+	}
+
+	for (key in mixed_var) {
+		if (mixed_var.hasOwnProperty(key)) {
+			cnt++;
+			if (mode == 1 && mixed_var[key] && (mixed_var[key].constructor === Array || mixed_var[key].constructor === Object)) {
+				cnt += this.count(mixed_var[key], 1);
+			}
+		}
+	}
+
+	return cnt;
 }
