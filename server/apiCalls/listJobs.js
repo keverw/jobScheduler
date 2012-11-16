1678c8c8d07484920e15b9f4fcda10bf3d9c1e17
Kevin Whitman <keverw92@gmail.com>
2012-11-16 13:47:04 -0500

    tiny bit of work on listsJob page

--- server/apiCalls/listJobs.js
+++ server/apiCalls/listJobs.js
@@ -7,8 +7,16 @@ module.exports = {
 		{
 			if (jobs.hasOwnProperty(jobID))
 			{
+				var jobInfo = {};
 				
-				console.log(jobID);
+				jobInfo.title = jobs[jobID].title;
+				jobInfo.url = jobs[jobID].url;
+				jobInfo.when = jobs[jobID].when;
+				jobInfo.added = jobs[jobID].added;
+				
+				//todo: add if it's pending(and when) or if it's running and when it started
+				
+				console.log(jobInfo);
 			}
 		}
 		
